db01673b2b79e318885741250112232652800d63
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-03-18 20:32:32 -0600

    Wrote code to get user's data and parse into hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,12 +8,20 @@ class Kele
     def initialize(username, password)
         response = self.class.post("/sessions", body: {email: username, password: password})
         if response && response["auth_token"]
+            @auth_token = response["auth_token"]
             puts "User #{username} has successfully logged in"
-            puts "Authentication token: #{response["auth_token"]}"
+            puts "Authentication token: #{@auth_token}"
         else
             puts "Invalid login!"
         end
     end
 
+    def get_me
+      response = self.class.get("/users/me", headers: {"authorization" => @auth_token })
+      @user_data = JSON.parse(response.body)
+      #puts @user_data
+      #puts "User id: #{@user_data["id"]}"
+      @user_data["id"]
+    end
 
 end
